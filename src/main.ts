0e9abf9e198d1f794441e516690f7f6200ffd9e4
TheLocagus <locagus@o2.pl>
2022-09-02 11:02:38 +0200

    Added frontUrl

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { frontUrl } from './utils/frontUrl';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://bkolsutjs-cv.networkmanager.pl' });
-  // app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: frontUrl });
   await app.listen(3001);
 }
 bootstrap();
